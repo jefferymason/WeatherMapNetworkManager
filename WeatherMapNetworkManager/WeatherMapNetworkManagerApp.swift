d60a3a03eb897be77de252b7477fdb1584982eae
jefferymason <jefferymason72@gmail.com>
2021-02-23 15:49:25 -0600

    Done

---
+++ WeatherMapNetworkManager/WeatherMapNetworkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMapNetworkManagerApp.swift
+//  WeatherMapNetworkManager
+//
+//  Created by Jeff Mason on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMapNetworkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
